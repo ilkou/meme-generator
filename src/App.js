8a29e1aab5f0e2eab7d7d5ffe11bc55d2651c4fe
ilkou <ouklichoussama@gmail.com>
2020-07-21 00:41:46 +0100

    realFetching-like management

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import Caption from './Caption.js';
 import 'react-bootstrap';
 import { Preloader, Oval } from 'react-preloader-icon';
 
+let reached_memes = 20;
+
 class App extends Component {
   constructor(props) {
       super(props);
       this.state = {
+          data: [],
           memes: []
       };
+      this.onScroll = this.onScroll.bind(this);
   }
   componentDidMount() {
       const apiUrl = 'https://api.imgflip.com/get_memes';
@@ -19,20 +23,51 @@ class App extends Component {
               const dataAsArray = Object.keys(data.data.memes).map((cid) => data.data.memes[cid]);
               //console.log(dataAsArray);
               let captions = [];
-              dataAsArray.forEach(meme => {
+              for (let i = 0; i < reached_memes; i++) {
+                  let meme = dataAsArray[i];
                   captions.push(<Caption link={meme.url} name={meme.name} key={meme.id}></Caption>);
-              });
+              }
+              // dataAsArray.forEach(meme => {
+              //     captions.push(<Caption link={meme.url} name={meme.name} key={meme.id}></Caption>);
+              // });
               this.setState({
+                  data: [...dataAsArray],
                   memes: [...captions]
-              })
+              });
               console.log(this.state.memes.length);
           });
   }
+  onScroll() {
+      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+          let captions = [];
+          for (let i = reached_memes; i < reached_memes + 10 && i < 100; i++) {
+              let meme = this.state.data[i];
+              captions.push(<Caption link={meme.url} name={meme.name} key={meme.id}></Caption>);
+          }
+          this.setState((prevState) => {
+              return {
+                  data: prevState.data,
+                  memes: prevState.memes.concat(captions)
+              }
+          });
+          reached_memes += 10;
+          if (reached_memes >= 100)
+              window.removeEventListener('scroll',this.onScroll);
+          //or u can show loading spinner and make fetch request to api
+      }
+  }
+  componentWillMount() {
+      window.addEventListener('scroll', this.onScroll);
+  }
+  componentWillUnmount() {
+      window.removeEventListener('scroll',this.onScroll);
+  }
 
   render() {
-    return (
+      return (
         <div className={"App"}>
             <h1>There's {this.state.memes.length} available memes</h1>
+            <small>scroll down for more :)</small>
             {
                 this.state.memes.length  === 0 ?
                     <div className={"Loading"}>
@@ -40,7 +75,7 @@ class App extends Component {
                         use={Oval}
                         size={100}
                         strokeWidth={10}
-                        strokeColor="#F0AD4E"
+                        strokeColor="#333333"
                         duration={800}
                         />
                     </div>
@@ -51,6 +86,7 @@ class App extends Component {
                 </div>
             </div>
             }
+            <small> {this.state.memes.length  === this.state.data.length ? "no more memes :) be creative and create one !" : ""}</small>
         </div>
     );
   }
